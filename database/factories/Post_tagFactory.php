f5d425125889c901d0abc8ddb53b9288e2cd2ceb
fahmihwan <fahmiiwan86@gmail.com>
2022-03-29 16:28:10 +0700

    ..

---
+++ database/factories/Post_tagFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class Post_tagFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'post_id' => 1,
+            'tag_id' => 1,
+        ];
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(2)->create();
+        \App\Models\Post::factory(5)->create();
+        \App\Models\Tag::factory(4)->create();
+        \App\Models\Post_tag::factory(2)->create();
+    }
+}
